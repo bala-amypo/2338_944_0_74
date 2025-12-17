13df0b7463f678c2e87c79a1ef0f6d9f57bf1a63
bala-amypo <bala@amypo.in>
2025-12-17 08:10:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,46 +1,58 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+
 
 public class Studententity {
-    private String name;
-    private int id;
-    private LocalDate date;
-    private float cgpa;
-
-    public Studententity(String name, int id, LocalDate date, float cgpa) {
-        this.name = name;
+    @Id
+    @Generatedvalue(strategy=GenerationType.IDENTITY);
+    private Long id;
+    @NotBlank(message= "should not contain spaces")
+    private  String name;
+     @NotBlank(message= "no blank allowed")
+     @Email(message="invalid format")
+    @Column(name=unique)
+    private String email;
+    private String password;
+    private String role;
+    public Studententity(Long id, String name, String email, String password, String role) {
         this.id = id;
-        this.date = date;
-        this.cgpa = cgpa;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
-    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public int getId() {
-        return id;
+    public String getEmail() {
+        return email;
     }
-
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
     public Studententity() {
     }
-    public void setId(int id) {
-        this.id = id;
+    public void setRole(String role) {
+        this.role = role;
     }
-    public LocalDate getDate() {
-        return date;
+    
     }
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
-      
-}
