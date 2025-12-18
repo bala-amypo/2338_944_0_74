a9b8f55457c508a469598027b86d4eb7108ea854
bala-amypo <bala@amypo.in>
2025-12-18 04:52:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,63 +1,41 @@
-package com.example.demo.entity;
+package com.example.project.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-@Entity
+import java.time.LocalDate;
 
 public class Studententity {
-    @Id
-    @Generatedvalue(strategy=GenerationType.IDENTITY);
-    private Long id;
-    @NotBlank(message= "should not contain spaces")
-    private  String name;
-    @NotBlank(message= "no blank allowed")
-    @Email(message="invalid format")
-    @Column(name=unique)
-    private String email;
-    private String password;
-    private String role;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String name;
+    private int id;
+    private LocalDate date;
+    private float cgpa;
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public String getEmail() {
-        return email;
+    public int getId() {
+        return id;
     }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-    
-    public Studententity(Long id, @NotBlank(message = "should not contain spaces") String name,
-            @NotBlank(message = "no blank allowed") @Email(message = "invalid format") String email, String password,
-            String role) {
+    public void setId(int id) {
         this.id = id;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+    public Studententity(String name, int id, LocalDate date, float cgpa) {
         this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
+        this.id = id;
+        this.date = date;
+        this.cgpa = cgpa;
     }
     public Studententity() {
     }
--- src/main/java/com/example/demo/newrepository/NewfileRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.jpa.repository.JpaRepository;
-import com.example.demo.entity.Studententity;
-public interface Newfilerepo extends JpaRepository<Studententity,Long>{
-
-}
